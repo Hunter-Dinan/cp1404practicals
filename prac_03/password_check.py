c8eb5da7da20fed55ac57c1e8adc9e25d53d58f2
Hunter <Hunter@daktech.com.au>
2021-03-20 12:33:59 +1000

    Add main function

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,9 +1,14 @@
 MIN_LENGTH = 5
 
-password = input("Enter password with minimum length of {}: ".format(MIN_LENGTH))
-while len(password) < MIN_LENGTH:
-    print("Invalid password")
-    password = input("Enter password: ")
 
-for i in range(0, len(password)):
-    print("*", end='')
+def main():
+    password = input("Enter password with minimum length of {}: ".format(MIN_LENGTH))
+    while len(password) < MIN_LENGTH:
+        print("Invalid password")
+        password = input("Enter password: ")
+
+    for i in range(0, len(password)):
+        print("*", end='')
+
+
+main()
