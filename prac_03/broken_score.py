b16e42473d0cad4a768fb3f935a0a8697ae0c176
Hunter <Hunter@daktech.com.au>
2021-03-20 15:00:15 +1000

    Refactor broken score and temperatures programs to use functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,30 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50 and score >= 0:
-    print("Bad")
-else:
-    print("Invalid score")
+
+def main():
+    score = float(input("Enter score: "))
+    grade = calculate_grade(score)
+    print(grade)
+
+    random_score = random.randint(0, 100)
+    random_score_grade = calculate_grade(random_score)
+    print(random_score_grade)
+
+
+def calculate_grade(score):
+    if score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    elif score < 50 and score >= 0:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
