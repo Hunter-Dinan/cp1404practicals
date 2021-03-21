3956761ecd719cbf8b41caaa3599c2fb97f4afc7
Hunter <Hunter@daktech.com.au>
2021-03-21 11:38:05 +1000

    Add security checker to list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,3 +11,10 @@ print("The last number is {}".format(numbers[-1]))
 print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the numbers is {}".format(average))
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
