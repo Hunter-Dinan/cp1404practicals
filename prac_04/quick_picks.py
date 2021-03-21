18233cdefda59b380985c8053229ba9a6003e55c
Hunter <Hunter@daktech.com.au>
2021-03-21 13:06:16 +1000

    Add quick picks program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,35 @@
+import random
+
+
+def main():
+    number_of_quick_picks = int(input("How many quick picks? "))
+    quick_picks = []
+    quick_picks = get_quick_picks(quick_picks, number_of_quick_picks)
+    sort_quick_picks(quick_picks)
+    print_quick_picks(quick_picks)
+
+
+def get_quick_picks(quick_picks: list, number_of_quick_picks):
+    for i in range(0, number_of_quick_picks):
+        numbers = []
+        for j in range(0, 6):
+            # store entire list in ith element of quick_pick_numbers
+            number = random.randint(1, 45)
+            while number in numbers:
+                number = random.randint(1, 45)
+            numbers.append(number)
+        quick_picks.append(numbers)
+    return quick_picks
+
+
+def sort_quick_picks(quick_picks: list):
+    for quick_pick in quick_picks:
+        quick_pick.sort()
+
+
+def print_quick_picks(quick_picks: list):
+    for quick_pick in quick_picks:
+        print("{:2} {:2} {:2} {:2} {:2}".format(quick_pick[0], quick_pick[1], quick_pick[2], quick_pick[3], quick_pick[4]))
+
+
+main()
