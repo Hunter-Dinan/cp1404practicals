b398c00e2878ea1f70fbe32125dbc136a0126e04
Hunter <Hunter@daktech.com.au>
2021-03-21 11:59:58 +1000

    Add list comprehensions program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -12,7 +12,8 @@ print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the numbers is {}".format(average))
 
-usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username = input("Username: ")
 if username in usernames:
     print("Access granted")
