237c60a119564008aee160ca164628e27499e0e7
Hunter <Hunter@daktech.com.au>
2021-03-28 12:27:37 +1000

    Add hex colours program

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,18 +1,16 @@
 """Look up hexadecimal colour codes."""
-# 10 colour names
-# AliceBlue
-# azure1
-# beige
-# blue1
-# blueviolet
-# CadetBlue
-# aquamarine1
-# chartreuse1
-# chocolate
-# CornflowerBlue
 
 COLOURS = {"aliceblue": "#f0f8ff", "azure1": "#f0ffff", "beige": "#f5f5dc",
            "blue1": "#0000ff", "blueviolet": "#8a2be2", "cadetblue": "#5f9ea0",
            "aquamarine1": "#7fffd4", "chartreuse1": "#7fff00",
            "chocolate": "#d2691e", "cornflowerblue": "#6495ed"}
 
+input_colour = input("Colour: ")
+
+while input_colour != "":
+    if input_colour.lower() in COLOURS:
+        print("{} hex code is {}".format(input_colour,
+                                         COLOURS[input_colour.lower()]))
+    else:
+        print("Invalid colour option. Try again")
+    input_colour = input("Colour: ")
