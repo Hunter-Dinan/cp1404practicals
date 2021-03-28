169afa5ecf380100f7930b2d4ccde31ce846d3dc
Hunter <Hunter@daktech.com.au>
2021-03-28 12:24:51 +1000

    Initialise hex colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,18 @@
+"""Look up hexadecimal colour codes."""
+# 10 colour names
+# AliceBlue
+# azure1
+# beige
+# blue1
+# blueviolet
+# CadetBlue
+# aquamarine1
+# chartreuse1
+# chocolate
+# CornflowerBlue
+
+COLOURS = {"aliceblue": "#f0f8ff", "azure1": "#f0ffff", "beige": "#f5f5dc",
+           "blue1": "#0000ff", "blueviolet": "#8a2be2", "cadetblue": "#5f9ea0",
+           "aquamarine1": "#7fffd4", "chartreuse1": "#7fff00",
+           "chocolate": "#d2691e", "cornflowerblue": "#6495ed"}
+
