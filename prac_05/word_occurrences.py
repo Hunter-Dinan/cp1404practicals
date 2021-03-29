ee6bea8e46fd096f3343809a6a85b0c1e2b1d985
Hunter <Hunter@daktech.com.au>
2021-03-29 17:20:34 +1000

    Fix dictionary names

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,4 +1,51 @@
 """Count the occurrences of words in a string."""
 
-words = {}
-string_input = input("Text: ")
+
+def main():
+    words = {}
+    string_input = input("Text: ")
+
+    word = ""
+    for i, char in enumerate(string_input):
+        string_length = len(string_input)
+        if char == " ":
+            store_word(words, word)
+            word = ""
+        elif i == string_length - 1:
+            word += char
+            store_word(words, word)
+        else:
+            word += char
+    sorted_words = sort_words(words)
+    display_words(sorted_words)
+    print(words.keys())
+
+
+def store_word(words: dict, word):
+    if word in words:
+        words[word] += 1
+    else:
+        words[word] = 1
+
+
+def display_words(words: dict):
+    words_list = []
+    for word in words:
+        words_list.append(word)
+    max_word_length = max(words_list)
+    print(max_word_length)
+    print("{} : {}".format(word, words[word]))
+
+
+def sort_words(words: dict):
+    words_list = []
+    sorted_words = {}
+    for word in words:
+        words_list.append(word)
+    words_list.sort()
+    for word in words_list:
+        sorted_words[word] = words[word]
+    return sorted_words
+
+
+main()
