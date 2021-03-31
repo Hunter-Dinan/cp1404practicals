ce1415f04a25b9c8a1b88064ad226e1a4eb7db6f
Hunter <Hunter@daktech.com.au>
2021-03-31 10:50:26 +1000

    Add word occurrences program

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -17,7 +17,8 @@ def main():
         else:
             word += char
     sorted_word_to_occurrence = get_sorted_dict(word_to_occurrence)
-    display_words(sorted_word_to_occurrence)
+    max_word_length = get_max_word_length(word_to_occurrence)
+    display_words(sorted_word_to_occurrence, max_word_length)
 
 
 def store_word_occurrences(word_to_occurrence: dict, word):
@@ -27,13 +28,12 @@ def store_word_occurrences(word_to_occurrence: dict, word):
         word_to_occurrence[word] = 1
 
 
-def display_words(word_to_occurrence: dict):
-    word_to_occurrence_list = []
+def display_words(word_to_occurrence: dict, max_word_length):
+    words = []
+    word_display_length = max_word_length + 2
     for word in word_to_occurrence:
-        word_to_occurrence_list.append(word)
-    # max_word_length = max(word_to_occurrence_list)
-    # print(max_word_length)
-        print("{} : {}".format(word, word_to_occurrence[word]))
+        words.append(word)
+        print("{:{}} {}".format(word + " :", word_display_length, word_to_occurrence[word]))
 
 
 def get_sorted_dict(word_to_occurrence: dict):
@@ -45,6 +45,16 @@ def get_sorted_dict(word_to_occurrence: dict):
     for word in words:
         sorted_word_to_occurrence[word] = word_to_occurrence[word]
     return sorted_word_to_occurrence
+
+
+def get_max_word_length(word_to_occurrence: dict):
+    words = []
+    word_lengths = []
+    for word in word_to_occurrence:
+        words.append(word)
+        word_lengths.append(len(word))
+    max_word_length = max(word_lengths)
+    return max_word_length
 
 
 main()
