6c88b0c46da6f09c7cfc18cc83c675b665419ca1
Hunter <Hunter@daktech.com.au>
2021-03-31 12:36:15 +1000

    Refactor emails program to use functions

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,24 +1,34 @@
-"""Store user's emails and names."""
+"""Store user's emails and names in a dictionary."""
 
-email_to_name = {}
-email = input("Email: ")
-while email != "":
-    name = email.split("@")[0]
-    email_to_name[email] = name.title()
-    if "." in email_to_name[email]:
-        names = email_to_name[email].split(".")
-        name = " ".join(names)
-        email_to_name[email] = name
-    valid_name = input("Is your name {}? (Y/n)".format(email_to_name[email]))
-    if valid_name.lower() == "n":
-        name = input("Name: ").title()
-        email_to_name[email] = name.title()
-    elif valid_name.lower() == "no":
-        name = input("Name: ").title()
-        email_to_name[email] = name.title()
-    print(email_to_name)
+
+def main():
+    """Create dictionary for emails and names."""
+    email_to_name = {}
     email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        valid_name = input("Is your name {}? (Y/n)".format(name))
+        if valid_name.lower() != "y" and valid_name.lower() != "":
+            name = input("Name: ").title()
+        email_to_name[email] = name.title()
+        email = input("Email: ")
+    print()
+    display_names_and_emails(email_to_name)
 
-print()
-for email, name in email_to_name.items():
-    print("{} ({})".format(name, email))
+
+def get_name_from_email(email):
+    """Get name from email address."""
+    name = email.split("@")[0]
+    if "." in name:
+        names = name.split(".")
+        name = " ".join(names)
+    return name.title()
+
+
+def display_names_and_emails(email_to_name: dict):
+    """Display names and their corresponding emails."""
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+main()
