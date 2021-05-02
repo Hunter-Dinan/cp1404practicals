36559e8072028dd2e0a29d3460e6a8b63f60fc0f
Hunter <Hunter@daktech.com.au>
2021-05-02 14:35:19 +1000

    Changed get_valid_number from function to static method and updated int typing to float typing

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,8 +1,8 @@
 """Module Docstring..."""
 
 from kivy.app import App
-from kivy.app import StringProperty
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.core.window import Window
 
 MILES_TO_KM_CONVERSION = 1.609
@@ -21,25 +21,25 @@ class ConvertMilesToKmApp(App):
 
     def handle_convert_m_to_km(self, input_number):
         """docstring..."""
-        miles_input = get_valid_number(input_number)
+        miles_input = self.get_valid_number(input_number)
         self.output_label = str(miles_input * MILES_TO_KM_CONVERSION)
 
-    def handle_increment(self, increment_value: int):
+    def handle_increment(self, increment_value):
         """docstring..."""
-        miles_input = get_valid_number(self.root.ids.input_number.text)
+        miles_input = self.get_valid_number(self.root.ids.input_number.text)
         miles_input += increment_value
 
         self.root.ids.input_number.text = str(miles_input)
-        self.handle_convert_m_to_km(miles_input)
+        # self.handle_convert_m_to_km(miles_input)
 
-
-def get_valid_number(number_input):
-    """docstring..."""
-    try:
-        number = int(number_input)
-    except ValueError:
-        number = 0
-    return number
+    @staticmethod
+    def get_valid_number(number_input):
+        """docstring..."""
+        try:
+            number = float(number_input)
+        except ValueError:
+            number = 0
+        return number
 
 
 ConvertMilesToKmApp().run()
