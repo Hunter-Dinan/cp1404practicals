ff9101d79499baa4e95f052add16f3566daad91f
Hunter <Hunter@daktech.com.au>
2021-05-02 14:23:07 +1000

    Add exception handling to inputs

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -19,17 +19,27 @@ class ConvertMilesToKmApp(App):
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_convert_m_to_km(self, input_number: int):
+    def handle_convert_m_to_km(self, input_number):
         """docstring..."""
-        self.output_label = str(input_number * MILES_TO_KM_CONVERSION)
+        miles_input = get_valid_number(input_number)
+        self.output_label = str(miles_input * MILES_TO_KM_CONVERSION)
 
     def handle_increment(self, increment_value: int):
         """docstring..."""
-        miles_input = int(self.root.ids.input_number.text)
+        miles_input = get_valid_number(self.root.ids.input_number.text)
         miles_input += increment_value
 
         self.root.ids.input_number.text = str(miles_input)
         self.handle_convert_m_to_km(miles_input)
+
+
+def get_valid_number(number_input):
+    """docstring..."""
+    try:
+        number = int(number_input)
+    except ValueError:
+        number = 0
+    return number
 
 
 ConvertMilesToKmApp().run()
