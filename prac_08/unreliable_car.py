599cc049e707b16a8e5ae1ed65430c0630b9ac1d
Hunter <Hunter@daktech.com.au>
2021-05-03 11:50:04 +1000

    Add reliability variable and create specialised drive method for UnreliableCar class

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -1,8 +1,19 @@
 """Unreliable Car class."""
 
 from car import Car
+import random
 
 
 class UnreliableCar(Car):
-    def __init__(self, name, fuel):
+    def __init__(self, name, fuel, reliability: float):
         super().__init__(name, fuel)
+        self.reliability = float(reliability)
+
+    def drive(self, distance):
+        random_number = random.randint(0, 100)
+        # only drive the car if that number is less than the car's reliability.
+        if random_number < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0
+        return distance_driven
--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -2,9 +2,17 @@
 
 from unreliable_car import UnreliableCar
 
-car_1 = UnreliableCar('Car 1', 100)
+car_1 = UnreliableCar('Car 1', 100, 84.3)
 print(car_1)
 car_1.add_fuel(1500)
 print(car_1)
 car_1.drive(1243)
+print(car_1)
+car_1.drive(30)
+print(car_1)
+car_1.drive(30)
+print(car_1)
+car_1.drive(30)
+print(car_1)
+car_1.drive(30)
 print(car_1)
