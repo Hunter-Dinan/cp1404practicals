6d8d52aff05b9ad36808206f7dbd759e0cdca37b
Hunter <Hunter@daktech.com.au>
2021-05-04 20:14:15 +1000

    Add sort files 1 program

---
+++ prac_09/sort_files_1.py
@@ -0,0 +1,48 @@
+"""Program that sorts files into directories based on their file type."""
+
+import os
+import os.path
+import shutil
+
+FILE_TYPE_INDEX = 1
+
+
+def main():
+    """Program that creates folders for each file type and stores the corresponding files within it."""
+    os.chdir('FilesToSort')
+    file_names = os.listdir('.')
+    file_types = get_file_types(file_names)
+    create_directories(file_types)
+    move_files_to_dir_by_type(file_names)
+
+
+def get_file_types(file_names: list):
+    """Return a list of file_types."""
+    file_types = []
+    for file_name in file_names:
+        if not os.path.isdir(file_name):
+            file_name_parts = file_name.split('.')
+            file_type = file_name_parts[FILE_TYPE_INDEX]
+            if file_type not in file_types:
+                file_types.append(file_type)
+    return file_types
+
+
+def create_directories(file_types: list):
+    """Create directories for each file type in the list."""
+    for file_type in file_types:
+        try:
+            os.mkdir(file_type)
+        except FileExistsError:
+            pass
+
+
+def move_files_to_dir_by_type(file_names: list):
+    """Move files to directories based on their type."""
+    for file_name in file_names:
+        if not os.path.isdir(file_name):
+            file_name_parts = file_name.split('.')
+            shutil.move(file_name, file_name_parts[FILE_TYPE_INDEX] + '/' + file_name)
+
+
+main()
